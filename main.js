bb3ccedeec7ad451e45bca326dbe5196568d4694
Lorenzo <lorenzo.franzone@gmail.com>
2020-01-31 13:42:21 +0100

    Start

---
+++ main.js
@@ -0,0 +1,20 @@
+// Api di Boolean... dischi musicali.
+// Servendoci di handlebars stampiamo tutto a schermo.
+// Creare una select con i seguenti generi: pop, rock, metal e jazz.
+// In base a cosa scegliamo nella select vedremo i corrispondenti cd.
+// Chiamata: https://flynn.boolean.careers/exercises/api/array/music
+
+$(document).ready(function(){
+
+  // Code
+
+
+
+  //////////////////////////////////////////////////
+  // F U N C T I O N S
+  //////////////////////////////////////////////////
+
+
+
+//////////
+});
