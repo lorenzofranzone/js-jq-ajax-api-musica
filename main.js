c22cf030ffb8535722b18532823d89ccdd76684a
Lorenzo <lorenzo.franzone@gmail.com>
2020-01-31 21:33:17 +0100

    HandleBars template

--- main.js
+++ main.js
@@ -8,11 +8,41 @@ $(document).ready(function(){
 
   // Code
 
+  var url = "https://flynn.boolean.careers/exercises/api/array/music";
+
+  // Chiamata Ajax
+  $.ajax(
+    {
+      url: url,
+      method: "GET",
+      // SUCCESS
+      success: function (data) {
+        // console.log(data);
+        processData(data.response);
+      },
+      // ERROR
+      error: function () {
+        alert("E' avvenuto un errore.");
+      }
+    }
+  );
 
 
   //////////////////////////////////////////////////
   // F U N C T I O N S
   //////////////////////////////////////////////////
+
+  function processData(album) {
+    for (var i = 0; i < album.length; i++) {
+      var singleAlbum = album[i];
+      // console.log(singleAlbum);
+      var source = $("#entry-template").html();
+      var template = Handlebars.compile(source);
+      var html = template(singleAlbum);
+      $('.cds-container').append(html);
+    }
+  }
+
 
 
 
